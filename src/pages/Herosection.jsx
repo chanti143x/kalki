ec17195a374c6c2e36306b0b570cfe29c9e6d801
chanti <chettisrirampatel@gmail.com>
2025-07-26 02:31:28 +0530

    explain it

--- src/pages/Herosection.jsx
+++ src/pages/Herosection.jsx
@@ -2,6 +2,7 @@ function HeroSection() {
   return (
     <>
       <div className="heroSection">
+
         <h4 className="title1">Hi i'am kalki</h4>
         <h2 className="title2">
           Lorem ipsum dolor <br />
