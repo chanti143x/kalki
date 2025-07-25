45df9913cddab9b8ef9f2999bd9865cc12a3abd3
Srivathsav-Kyatham <2023.srivathsavk@isu.ac.in>
2025-07-26 02:14:54 +0530

    add span

--- src/pages/Herosection.jsx
+++ src/pages/Herosection.jsx
@@ -1,29 +1,15 @@
-function HeroSection(){
-
-    return(
-
-        <>
-        
-        <div className="heroSection">
-
-            <h4 className="title1">Hi i'am kalki</h4>
-            <h2 className="title2">Lorem ipsum dolor <br/> <span>
-                
-                
-                
-                </span>sit amet consectetur.</h2>
-
-        </div>
-        
-        </>
-
-
-
-
-
-
-
-    )
+function HeroSection() {
+  return (
+    <>
+      <div className="heroSection">
+        <h4 className="title1">Hi i'am kalki</h4>
+        <h2 className="title2">
+          Lorem ipsum dolor <br />
+          <span className="hello">sit amet consectetur.</span>
+        </h2>
+      </div>
+    </>
+  );
 }
 
 export default HeroSection;
